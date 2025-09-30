7c608c916a6917ed35d2bb4e3b7e1c15b72dd948
WaltBox <98342450+WaltBox@users.noreply.github.com>
2025-09-29 21:07:36 -0500

    update

--- src/components/UnfairnessSection.jsx
+++ src/components/UnfairnessSection.jsx
@@ -195,7 +195,7 @@ const UnfairnessSection = ({ reviews, reviewsStats, reviewsLoading }) => {
           </div>
           
           {/* Phone Frame */}
-          <div className="relative bg-black rounded-[3rem] p-1 shadow-2xl" style={{ aspectRatio: '9/19.5' }}>
+          <div className="relative bg-black rounded-[3rem] p-1 shadow-2xl w-80 h-[650px]">
             {/* Dynamic Island */}
             <div className="absolute top-2 left-1/2 transform -translate-x-1/2 w-28 h-7 bg-black rounded-full z-10"></div>
             
@@ -249,10 +249,10 @@ const UnfairnessSection = ({ reviews, reviewsStats, reviewsLoading }) => {
               </div>
 
               {/* Messages Container */}
-              <div className="flex-1 overflow-y-auto bg-white p-4 space-y-1 relative">
+              <div className="flex-1 overflow-y-auto bg-white p-4 space-y-1 relative min-h-[400px]">
                 {displayedMessages.length === 0 && !isSimulationRunning ? (
                   // Empty thread state
-                  <div className="flex flex-col items-center justify-center h-full text-center py-20">
+                  <div className="flex flex-col items-center justify-center h-full text-center py-8">
                     <div className="w-16 h-16 bg-gray-100 rounded-full flex items-center justify-center mb-4">
                       <svg className="w-8 h-8 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />

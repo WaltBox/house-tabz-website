8ca1a9adfae9c3eda5b4108cbb10418e7de4a2f9
WaltBox <98342450+WaltBox@users.noreply.github.com>
2025-10-04 18:39:15 -0500

    update

--- src/components/MetWaltPage.jsx
+++ src/components/MetWaltPage.jsx
@@ -283,11 +283,10 @@ const MetWaltPage = () => {
             </p>
             
             <p>
-              I believe many people choose to live alone because they crave independence. However, living alone is a net negative and leads to isolation and loneliness. With HouseTabz, my mission is to bring the same sense of personal responsibility, while making it easier and more rewarding to live together.
-            </p>
+              I believe people choose to live alone because they crave independence. I think that living alone is not healty and leads to isolation and loneliness. With HouseTabz, my mission is to bring the same sense of independence, while making it easier and more rewarding to live together.
             
             <p className="font-semibold text-[#34d399] text-center pt-4 border-t border-gray-100">
-              Soon, sharing expenses will not contribute to a negative perception of shared living.
+              Shared expenses should not contribute to a negative perception of shared living.
             </p>
           </div>
         </div>
